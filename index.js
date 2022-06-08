3587618f45c137701da4a4bce1abe088b4d308dd
Luciano Steck <lusteck@hotmail.com>
2022-06-08 12:33:53 -0300

    fix: braspag alteration in index

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules, Platform } from "react-native";
 const { RNCybersourceDeviceFingerprint } = NativeModules;
 
 var Status = {
@@ -24,38 +23,38 @@ var Status = {
   ENDNOTIFIERNOTFOUND: 18, // unique for android
   INQUIETPERIOD: 19, // unique for android
   properties: {
-    0: {name: "notyet", value: 0},
-    1: {name: "ok", value: 1},
-    2: {name: "connectionerror", value: 2},
-    3: {name: "hostnotfounderror", value: 3},
-    4: {name: "networktimeouterror", value: 4},
-    5: {name: "hostverificationerror", value: 5},
-    6: {name: "internalerror", value: 6},
-    7: {name: "interruptederror", value: 7},
-    8: {name: "partialprofile", value: 8},
-    9: {name: "invalidorgid", value: 9},
-    10: {name: "notconfigured", value: 10},
-    11: {name: "certificatemismatch", value: 11},
-    12: {name: "invalidparameter", value: 12},
-    13: {name: "strongauthfailed", value: 13},
-    14: {name: "strongauthcancelled", value: 14},
-    15: {name: "strongauthunsupported", value: 15},
-    16: {name: "strongauthusernotfound", value: 16},
-    17: {name: "blocked", value: 17},
-    18: {name: "endnotifiernotfound", value: 18},
-    19: {name: "inquietperiod", value: 19},
+    0: { name: "notyet", value: 0 },
+    1: { name: "ok", value: 1 },
+    2: { name: "connectionerror", value: 2 },
+    3: { name: "hostnotfounderror", value: 3 },
+    4: { name: "networktimeouterror", value: 4 },
+    5: { name: "hostverificationerror", value: 5 },
+    6: { name: "internalerror", value: 6 },
+    7: { name: "interruptederror", value: 7 },
+    8: { name: "partialprofile", value: 8 },
+    9: { name: "invalidorgid", value: 9 },
+    10: { name: "notconfigured", value: 10 },
+    11: { name: "certificatemismatch", value: 11 },
+    12: { name: "invalidparameter", value: 12 },
+    13: { name: "strongauthfailed", value: 13 },
+    14: { name: "strongauthcancelled", value: 14 },
+    15: { name: "strongauthunsupported", value: 15 },
+    16: { name: "strongauthusernotfound", value: 16 },
+    17: { name: "blocked", value: 17 },
+    18: { name: "endnotifiernotfound", value: 18 },
+    19: { name: "inquietperiod", value: 19 },
   },
-  init: function(rawValue) {
-    var mappedValue = rawValue
-    if (Platform.OS == 'android') {
+  init: function (rawValue) {
+    var mappedValue = rawValue;
+    if (Platform.OS == "android") {
       // This will map the index / value of the android enum ordinal to the ios enum ordinal
       // E.g.: The ordinal for 'internal error' in android is 5, in ios it is 6, so we map 5 to 6
-      map = {5: 6, 6: 5, 8: 10, 9: 8, 13: 11, 14: 13, 15: 14, 16: 15}
-      mappedValue = map[rawValue] ? map[rawValue] : rawValue
+      map = { 5: 6, 6: 5, 8: 10, 9: 8, 13: 11, 14: 13, 15: 14, 16: 15 };
+      mappedValue = map[rawValue] ? map[rawValue] : rawValue;
     }
 
-    return Status.properties[mappedValue]
-  }
+    return Status.properties[mappedValue];
+  },
 };
 
 /**
@@ -65,25 +64,27 @@ var Status = {
  *     documentation.
  * @return {promise} Promise resolves true if call is done, otherwise throws.
  */
-function configure(orgId) {
-  return RNCybersourceDeviceFingerprint.configure(orgId);
+function configure(orgId, serverURL) {
+  return RNCybersourceDeviceFingerprint.configure(orgId, serverURL);
 }
 
 /**
  * Profile request
- * @param {Array.<string>} attributes A list of custom attributes
+ * @param {string} fingerprintKey A list of custom attributes
  * @return {promise} An object containing a sessionId, and a status (Status enum)
  */
-function getSessionID(attributes) {
-  return RNCybersourceDeviceFingerprint.getSessionID(attributes).then(result => {
-    return {
-      sessionId: result.sessionId,
-      status: Status.init(result.status),
+function getSessionID(fingerprintKey) {
+  return RNCybersourceDeviceFingerprint.getSessionID(fingerprintKey).then(
+    (result) => {
+      return {
+        sessionId: result.sessionId,
+        status: Status.init(result.status),
+      };
     }
-  })
+  );
 }
 
 module.exports = {
   configure,
   getSessionID,
-}
+};
